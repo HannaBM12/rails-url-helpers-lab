bf037777228b49bcd168272efcdf767e031ef8a0
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-18 00:13:33 -0400

    commit tree 8ff796c1c0e9ae24d1b6c852516a098dcd74abb0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,23 +1,49 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  # before_action :set_student, only: :show
   
-  def index
-    @students = Student.all
-  end
-
-  def show
-    set_student
-  end
-
-  def activate_student_path
-    set_student
-    @student.active = !@student.active
-    @student.save 
-    redirect_to student_path(@student)
-  end
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
+    def index
+      @students = Student.all
     end
+
+    def show
+      @student= Student.find(params[:id])
+    end
+
+    def activate_student_path
+      @student = Student.find(params[:id])
+      @student.active = !@student.active
+      @student.save
+      redirect_to student_path(@student)
+    end
+
+
+
+
+
+
+
+
+
+
+
 end
+#   def index
+#     @students = Student.all
+#   end
+
+#   def show
+#     set_student
+#   end
+
+#   def activate_student_path
+#     set_student
+#     @student.active = !@student.active
+#     @student.save 
+#     redirect_to student_path(@student)
+#   end
+#   private
+
+#     def set_student
+#       @student = Student.find(params[:id])
+#     end
+# end
